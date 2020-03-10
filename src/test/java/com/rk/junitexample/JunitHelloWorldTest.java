6ebdc70f68d883324b21f7945166f76ac9292c0b
Rajesh Khatwa <rajesh.kumar.khatwa@sap.com>
2020-03-10 13:19:49 +0530

    Staged branch

--- src/test/java/com/rk/junitexample/JunitHelloWorldTest.java
+++ src/test/java/com/rk/junitexample/JunitHelloWorldTest.java
@@ -10,7 +10,7 @@ public class JunitHelloWorldTest {
 	public void isGreaterTest() {
 		NumberCheck JunitTest = new NumberCheck(5, 3);
 		assertTrue(JunitTest.isGreater(), "Num1 is greater than num2 ");
-		System.out.println("I am test modified in Github and then modified in Eclipse");
+		System.out.println("I am test modified in Github and then modified in Eclipse in the new staged branch");
 	}
 
 }
