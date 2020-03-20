d5bd764b574331090f66fe023c514c4c7be27125
rajeshkhatwa <61071234+rajeshkhatwa@users.noreply.github.com>
2020-03-20 08:42:31 +0530

    Update JunitHelloWorld.java
    er

--- src/main/java/com/rk/junitexample/JunitHelloWorld.java
+++ src/main/java/com/rk/junitexample/JunitHelloWorld.java
@@ -7,7 +7,7 @@ package com.rk.junitexample;
 public class JunitHelloWorld {
 	public static void main(String[] args) {
 		NumberCheck checkGreater = new NumberCheck(3, 4);
-		System.out.println("Num1 is greater than num 2 as informed by Rajesh " + checkGreater.isGreater());
+		System.out.println("Num1 is greater than num 2 as informed by Rajesh again chanege " + checkGreater.isGreater());
 
 	}
 }
